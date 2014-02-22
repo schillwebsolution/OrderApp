7833a51c50c14db5814eed64bcb03d7737496cb7
Sven Schill <sven.schill@schill-websolution.de>
2014-02-22 15:25:58 +0100

    first pages

--- js/dataController.js
+++ js/dataController.js
@@ -17,28 +17,32 @@ function getData(){
 
 function drawPages(DATA){
 	drawCategoryOverview(DATA);
-	/*for (categoryId in DATA) {
-		drawProducts(getProducts(categoryId));	
-	}*/
 }
 
 function drawCategoryOverview(data){
 	var li = "";
-	jQuery.each(data, function(i, c) {
-		li += "<li><a href='#category" + c.name + "'>" + c.name + "</a></li>";
+	$.each(data, function(i, c) {
+		$.each(c, function(j, category){
+			li += "<li><a href='#category" + category["@attributes"].id + "'>" + category.name + "</a></li>";
+			drawProducts(category["@attributes"].id, category.products);
+		});
 	});
 	var htmlString = 
 		"<ul data-role='listview' data-inset='true'>" + li + "</ul>";
-		$('#catArea').html(htmlString);
+		$('#categories > div[data-role="content"]').html(htmlString);
+		$('#categories > div[data-role="content"] > ul').listview();
 }
 
-function getProducts(category){
-	return Array();	
-}
-
-
-
-function drawProducts(products){
+function drawProducts(cId, products){
+	var htmlString = "<div data-role='page' id='category" + cId + "'><div data-role='content'></div><div data-role='footer'><h4>Fußzeile</h4></div></div>";
+	$('body').html($('body').html() + htmlString);
+	var productHtml = "";
+	$.each(products, function(i, p){
+		$.each(p, function(j, product){
+			productHtml += "<div data-role='collapsible' data-inset='false'><h4> " + product.name + "</h4><p> " + product.shortDesc + " </p></div>"
+		})
+	});
 	
+	$('#category' + cId + ' > div[data-role="content"]').html(productHtml);	
 }
 
