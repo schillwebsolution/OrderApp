98e6befd4fbcb046433718c38baa1cf057f901af
Sven Schill <sven.schill@schill-websolution.de>
2014-02-22 01:25:17 +0100

    init settings

--- js/dataController.js
+++ js/dataController.js
@@ -1,12 +1,44 @@
 // JavaScript Document
-
+$(document).ready(function(){
+	getData();
+});
 function getData(){
-	return Array();
+	$.ajax({
+		url : "http://schill-websolution.de/services/orderapp/data.php",
+		jsonp:"callback",
+		dataType: "jsonp",
+		data: "",
+		success: function( DATA ) {
+			drawPages(DATA)	;
+		},
+		jsonpCallback: "callback"
+	});	
 }
-var DATA = {
-	tree: getData()
+
+function drawPages(DATA){
+	drawCategoryOverview(DATA);
+	/*for (categoryId in DATA) {
+		drawProducts(getProducts(categoryId));	
+	}*/
+}
+
+function drawCategoryOverview(data){
+	var li = "";
+	jQuery.each(data, function(i, c) {
+		li += "<li><a href='#category" + c.name + "'>" + c.name + "</a></li>";
+	});
+	var htmlString = 
+		"<ul data-role='listview' data-inset='true'>" + li + "</ul>";
+		$('#catArea').html(htmlString);
 }
 
 function getProducts(category){
 	return Array();	
 }
+
+
+
+function drawProducts(products){
+	
+}
+
--- js/site.js
+++ js/site.js
@@ -1,15 +1,1 @@
-// JavaScript Document
-$(document).ready(function(){
-	drawCategoryOverview(DATA.tree);
-	for (categoryId in DATA.tree.category) {
-		drawProducts(getProducts(categoryId));	
-	}
-});
 
-function drawCategoryOverview(data){
-	
-}
-
-function drawProducts(products){
-	
-}
